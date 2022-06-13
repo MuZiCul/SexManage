2fb090296e5928a0d557dec456d8ad07c1e2b3d9
muzicul <Obama110@163.com>
2022-06-13 18:06:45 +0800

    解决重启后前端session影响后端session的问题

--- config/decorators.py
+++ config/decorators.py
@@ -1,4 +1,4 @@
-from flask import g, redirect, url_for
+from flask import g, redirect, url_for, session, render_template
 from functools import wraps
 
 
@@ -6,7 +6,13 @@ def login_required(fun):
     @wraps(fun)
     def wrapper(*args, **kwargs):
         if hasattr(g, 'user'):
-            return fun(*args, **kwargs)
+            try:
+                print(g.user.id)
+                return fun(*args, **kwargs)
+            except Exception as e:
+                print('前端session未清空：', str(e))
+                session.clear()
+                return render_template('login.html')
         else:
             return redirect(url_for('login.login'))
     return wrapper
